f59ff8785fdac29c7eb2248a3d64b27c27830819
Artiom Bozieac <artiomytprofile@gmail.com>
2021-03-29 12:56:26 +0300

    Included Tetris class

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,10 @@
-﻿#include <SFML/Graphics.hpp>
-#include <SFML/Main.hpp>
-#include <iostream>
+﻿#include "headers/Tetris.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(480, 640), "Tetris");
+    Tetris game;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.display();
-    }
+    game.run();
 
     return 0;
 }
